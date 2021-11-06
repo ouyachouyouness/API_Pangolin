80ee84fea771431274946908fbd1d85c8e7a17ee
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-11-06 14:13:48 +0100

    app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send({ message: "salam" });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`app is rinning on port ${port}`));
